c2469403874fe02dff2fa83227519d3f303af315
RenatoMartinsFerreira <linkrenato11@gmail.com>
2020-04-28 01:22:05 -0300

    Utilizando react-native-config.

--- webjumpMovieListApp/src/services/baseHttpService.js
+++ webjumpMovieListApp/src/services/baseHttpService.js
@@ -1,13 +1,13 @@
 import axios from 'axios';
+import Config from 'react-native-config';
+
 export default class BaseHttpService {
   constructor() {
     this.header = {
       headers: {
         'Content-Type': 'application/json',
         'trakt-api-version': 2,
-        'trakt-api-key':
-          'c7c84f83dfddcfdf9ba661d7600a538a66681b19d811f23331b13870e5829c1e',
-        api_key: '96c13e60658078293b1c011827aaa291',
+        'trakt-api-key': Config.TRAKT_API_KEY,
       },
     };
   }
